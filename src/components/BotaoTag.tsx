b5c49bb564043ef26a393a6e3cd6b0b3549df1e5
leo <leonardo.private@hotmail.com>
2024-06-21 20:39:53 -0300

    temp(placeholder): add temporary placeholder for videos

--- src/components/BotaoTag.tsx
+++ src/components/BotaoTag.tsx
@@ -5,6 +5,6 @@ interface BotaoNome {
 
 export default function BotaoTag({ nome, cor }:BotaoNome) {
   return(
-    <h1 className="w-1/3 text-center rounded-xl p-4 font-bold md:w-1/2 lg:w-1/6" style={{backgroundColor: cor}}>{nome.toUpperCase()}</h1>
+    <h1 className="w-1/3 mt-1 text-center rounded-xl p-4 font-bold md:w-1/2 lg:w-1/6" style={{backgroundColor: cor}}>{nome.toUpperCase()}</h1>
   )
 }
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -2,12 +2,36 @@ import BotaoTag from "./BotaoTag";
 
 export default function Conteudo() {
   return (
-    <section>
+    <section className="overflow-hidden">
       <section className="flex flex-col items-center justify-around h-screen bg-zinc-900">
         <BotaoTag nome="Front End" cor="#86d0fe" />
-        {/* front end content */}
+        <iframe
+        className="m-2"
+          width="300"
+          height="167"
+          src="https://www.youtube.com/embed/pIvHp7sk2UA?si=nz3IWYgRWXzOC295"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
         <BotaoTag nome="Back End" cor="#5ec376" />
+        <iframe
+        className="m-2"
+          width="300"
+          height="167"
+          src="https://www.youtube.com/embed/pIvHp7sk2UA?si=nz3IWYgRWXzOC295"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
         <BotaoTag nome="Mobile" cor="#f4bb26" />
+        <iframe
+        className="m-2"
+          width="300"
+          height="167"
+          src="https://www.youtube.com/embed/pIvHp7sk2UA?si=nz3IWYgRWXzOC295"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          
+        ></iframe>
       </section>
     </section>
   );
